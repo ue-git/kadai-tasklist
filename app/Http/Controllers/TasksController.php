a934bab100a2918f39659fedcf899b192e483171
ue <git.ue.develop@gmail.com>
2020-07-24 09:11:32 +0000

    statusカラムの追加とvalidationへの対応追加

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,7 +42,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status'=>'required|max:10',
+            'content'=>'required|max:255',
+        ]);
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         return redirect('/');
@@ -85,7 +90,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status'=>'required|max:10',
+            'content'=>'required|max:255',
+        ]);
         $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         return redirect('/');
